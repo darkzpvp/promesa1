55f9e495f5778dfe24df26eb7f10374b60142ec8
darkzpvp <victor1val@hotmail.es>
2023-11-27 19:56:28 +0100

    funciones

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
+
 txt = document.querySelector("#txt");
 json_objeto = document.querySelector("#json-objeto");
 json_array = document.querySelector("#json-array");
 cargarapi = document.querySelector("#cargarapi");
 resultado = document.querySelector("#resultado");
-
-txt.addEventListener("click", cargarDesdeTxt);
-json_objeto.addEventListener("click", cargarJSONObjeto);
-json_array.addEventListener("click", () => {
-  resultado.innerHTML = "";
-  if (resultado.innerHTML.trim() == "") {
-    cargarJSONArray();
-  }
-  
-});
-
-cargarapi.addEventListener("click", cargarAPI);
-
+// Función para manejar operaciones genéricas de fetch
+function fetchData(url, callback) {
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error");
+      } else {
+        return response.json(); // Suponiendo que la respuesta siempre es JSON
+      }
+    })
+    .then((data) => {
+      callback(data);
+    })
+    .catch((error) => {
+      console.error("Error" + error);
+    });
+    restablecer();
+}
+function restablecer(){
+  resultado.textContent="";
+}
+// Función para cargar datos desde un archivo de texto
 function cargarDesdeTxt() {
   const url = "/promesas/ejercicios/promesa1/datos.txt";
   fetch(url)
@@ -33,84 +43,56 @@ function cargarDesdeTxt() {
       console.error("Error" + error);
     });
 }
-
+// Función para cargar un objeto JSON
 function cargarJSONObjeto() {
   const url = "/promesas/ejercicios/promesa1/empleado.json";
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Error");
-      } else {
-        return response.json();
-      }
-    })
-    .then((data) => {
-      resultado.innerHTML = JSON.stringify(data);
-    })
-    .catch((error) => {
-      console.error("Error" + error);
-    });
+  fetchData(url, (data) => {
+    resultado.innerHTML = JSON.stringify(data);
+  });
 }
 
+// Función para cargar un array JSON
 function cargarJSONArray() {
   const url = "/promesas/ejercicios/promesa1/empleados.json";
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Error");
-      } else {
-        return response.json();
-      }
-    })
-    .then((data) => {
-      data.forEach((datos) => {
-        const nuevoElemento = document.createElement("p");
-        nuevoElemento.innerHTML = JSON.stringify(datos);
-        resultado.appendChild(nuevoElemento);
-      });
-    })
-    .catch((error) => {
-      console.error("Error" + error);
+  fetchData(url, (data) => {
+    data.forEach((datos) => {
+      const nuevoElemento = document.createElement("p");
+      nuevoElemento.innerHTML = JSON.stringify(datos);
+      resultado.appendChild(nuevoElemento);
     });
+  });
 }
 
+// Función para cargar datos desde una API
 function cargarAPI() {
-    const url = "https://picsum.photos/list";
-    fetch(url)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Error");
-            } else {
-                return response.json();
-            }
-        })
-        .then((data) => {
-            data.forEach((datos) => {
-    
-                const authorUrl = datos.author_url;
-                const nuevoElemento2 = document.createElement("a");
-                nuevoElemento2.href = authorUrl;
-                nuevoElemento2.textContent = "Ver Imagen";
-                const nuevoElemento = document.createElement("div");
+  const url = "https://picsum.photos/list";
+  fetchData(url, (data) => {
+    data.forEach((datos) => {
+      const authorUrl = datos.author_url;
+      const nuevoElemento2 = document.createElement("a");
+      nuevoElemento2.href = authorUrl;
+      nuevoElemento2.textContent = "Ver Imagen";
+      const nuevoElemento = document.createElement("div");
 
-        
-                nuevoElemento.innerHTML = `
-                    <strong>Author:</strong> ${datos.author}<br>
-                    <strong>Post:</strong> ${datos.post_url}<br>
-                    <strong>ID:</strong> ${datos.id}<br>
-                    <strong>Width:</strong> ${datos.width}<br>
-                    <strong>Height:</strong> ${datos.height}<br>
-                    ${nuevoElemento2.outerHTML}<br> 
-                    <strong>Filename:</strong> ${datos.filename}<br>
-                    <strong>Format:</strong> ${datos.format}<br>
-                `; //outerHTML es para que no me lo imprima como cadena, sino como HTML, ya que la intención es que sea un href que ponga ver imagen
+      nuevoElemento.innerHTML = `
+        <strong>Autor:</strong> ${datos.author}<br>
+        <strong>Publicación:</strong> ${datos.post_url}<br>
+        <strong>ID:</strong> ${datos.id}<br>
+        <strong>Ancho:</strong> ${datos.width}<br>
+        <strong>Alto:</strong> ${datos.height}<br>
+        ${nuevoElemento2.outerHTML}<br> 
+        <strong>Nombre de archivo:</strong> ${datos.filename}<br>
+        <strong>Formato:</strong> ${datos.format}<br>
+      `;//OuterHTML lo uso para que me imprima el HTML tal y como es, si no no aparecería el href ni el contenido
 
-                
-                resultado.appendChild(nuevoElemento);
-            });
-        })
-        .catch((error) => {
-            console.error("Error" + error);
-        });
-        resultado.innerHTML = "";
+      resultado.appendChild(nuevoElemento);
+    });
+  });
+  resultado.innerHTML = "";
 }
+
+// Event listeners
+txt.addEventListener("click", cargarDesdeTxt);
+json_objeto.addEventListener("click", cargarJSONObjeto);
+json_array.addEventListener("click", cargarJSONArray);
+cargarapi.addEventListener("click", cargarAPI);
